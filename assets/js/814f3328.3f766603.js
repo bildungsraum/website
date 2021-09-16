7beb17c5a7a1c0ea2dff6577aac3a7543be21d63
janrenz <bildungsraum@janrenz.de>
2021-09-16 17:28:27 +0000

    Deploy website - based on c08f957435a1fb1d6a5983d1d40f24bc48bed70a

---
+++ assets/js/814f3328.3f766603.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknbp_docs=self.webpackChunknbp_docs||[]).push([[535],{5641:function(t){t.exports=JSON.parse('{"title":"Letzte Beitr\xe4ge","items":[{"title":"Erstes Pilotprojekt f\xfcr Nationale Bildungsplattform startet","permalink":"/website/blog/bird-startet"}]}')}}]);
--- assets/js/814f3328.8f3641e5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknbp_docs=self.webpackChunknbp_docs||[]).push([[535],{5641:function(t){t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Erstes Pilotprojekt f\xfcr Nationale Bildungsplattform startet","permalink":"/website/blog/bird-startet"}]}')}}]);
