28d3c1dae57bde7ee5dd9d3f963a3a31dab5bd72
Jan Renz <jan.renz@gmail.com>
2021-09-16 19:08:35 +0200

    first real blog post

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,13 +26,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/bildungsraum/website/edit/master/docs/',
+          editUrl: 'https://github.com/bildungsraum/website/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/bildungsraum/website/edit/master/website/blog/',
+            'https://github.com/bildungsraum/website/edit/master//blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
